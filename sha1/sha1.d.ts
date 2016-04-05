cf6a06ec28d9c96985d80264b98ad406bdefc8d8
Oliver Ridgway <oli@kineticfaction.co.uk>
2016-04-05 10:30:03 +0100

    Added Definitions for SHA1
    Added Definitions for SHA1 from https://github.com/pvorb/node-sha1

---
+++ sha1/sha1.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for sha1 1.1.1
+// Project: https://github.com/pvorb/node-sha1
+// Definitions by: Oliver Ridgway <https://github.com/kineticfaction/>
+
+declare function sha1 (message: string): string;
+
+export = sha1
