0b2767b581aedf2edeb26b02b9db51718ed588a3
Oliver Ridgway <oli@kineticfaction.co.uk>
2016-04-05 10:35:40 +0100

    Added comments
    Added comment docblock

--- sha1/sha1.d.ts
+++ sha1/sha1.d.ts
@@ -2,6 +2,9 @@
 // Project: https://github.com/pvorb/node-sha1
 // Definitions by: Oliver Ridgway <https://github.com/kineticfaction/>
 
+/**
+ * Returns the SHA-1 hash of the given message. 
+ */
 declare function sha1 (message: string): string;
 
 export = sha1
