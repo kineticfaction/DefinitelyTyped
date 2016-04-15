f8fdd1d9882a53cdd2b1f4b438c312683874bcdd
Oliver Ridgway <oli@kineticfaction.co.uk>
2016-04-15 15:29:57 +0100

    Create validatorjs.d.ts

---
+++ ValidatorJS/validatorjs.d.ts
@@ -0,0 +1,15 @@
+// Type definitions for ValidatorJS
+// Definitions by: Oliver Ridgway <https://github.com/kineticfaction/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference path="../express/express.d.ts" />
+/// <reference path="../node/node.d.ts" />
+
+
+declare namespace ValidatorJS {
+    
+    export interface ValidatorStatic {
+        
+    }
+    
+}
